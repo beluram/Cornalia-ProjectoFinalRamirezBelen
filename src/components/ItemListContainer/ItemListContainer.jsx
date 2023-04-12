5e24c4e96093cf105058f0f5e0dcbc07bd656f33
beluram <belenram1996@gmail.com>
2023-04-12 12:50:56 -0300

    cambio en itemlist para category

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,26 +1,35 @@
 import styles from './ItemListContainer.module.css';
 import Card from '../Card/Card';
 import { useEffect, useState } from 'react';
-import { collection, getDocs } from "firebase/firestore"
+import { collection, getDocs, query, where } from "firebase/firestore";
 import db from "../../../db/firebase-config" ;
-//import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 const ItemListContainer = () => {
   
   const [items, setItems ] = useState([]);
   const itemsRef = collection(db, "items");
-
+   const {id} = useParams() 
   const getItems = async () => {
     const itemsCollection = await getDocs(itemsRef);
-    const items = itemsCollection.docs.map((doc) => ({...doc.data(), id: doc.id, id: doc.category}));
+    const items = itemsCollection.docs.map((doc) => ({...doc.data(), id: doc.id}));
     setItems(items);
   };
 
-  console.log(items)
+const getItemsCategory = async () =>{ 
+      const q = query(collection(db,"items"),where('category','==',id))
+      const itemsCollection = await getDocs(q)
+      const itemsFiltrados = itemsCollection.docs.map((doc) => doc = { ...doc.data(),id:doc.id})
+      setItems(itemsFiltrados) 
+  }   
 
   useEffect(() =>{
-    getItems();
-  }, []);
+    if(id){
+      getItemsCategory()
+    }else{
+      getItems();
+    }
+  }, [id]);
 
       return (
         <div className={styles.container}>
